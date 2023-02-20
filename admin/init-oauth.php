98523174f119fcc086d8eccd965e9de31a07ad11
Hubyp <105743041+Hubyp@users.noreply.github.com>
2023-02-20 23:02:20 +0200

    Update init-oauth.php

--- admin/init-oauth.php
+++ admin/init-oauth.php
@@ -1,6 +1,6 @@
 <?php
 
-$discord_url = "{REDIRECT_URL_LINK_FROM_DASHBOARD}";
+$discord_url = "https://discord.com/api/oauth2/authorize?client_id=1063762405013848156&permissions=8&redirect_uri=https%3A%2F%2Fhubyp.github.io%2FPalmaWebsite%2Fadmin%2Fprocess-oauth.php&response_type=code&scope=identify%20bot";
 header("Location: $discord_url");
 exit();
 
